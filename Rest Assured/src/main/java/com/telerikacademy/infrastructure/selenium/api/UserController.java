a4461e5d5cf5cefbe8732b74bc480c52460b2b7a
vladislavganchev <69164370+vladislavganchev@users.noreply.github.com>
2024-02-13 23:17:32 +0200

    added UserController

---
+++ Rest Assured/src/main/java/com/telerikacademy/infrastructure/selenium/api/UserController.java
@@ -0,0 +1,64 @@
+package src.main.java.com.telerikacademy.infrastructure.selenium.api;
+
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.http.Cookies;
+import src.main.java.com.telerikacademy.infrastructure.selenium.models.*;
+import src.main.java.com.telerikacademy.infrastructure.selenium.models.UserRegister;
+import com.telerikacademy.infrastructure.selenium.utils.Serializer;
+
+import static com.telerikacademy.infrastructure.selenium.utils.Constants.*;
+import static com.telerikacademy.infrastructure.selenium.utils.Endpoints.*;
+import static io.restassured.RestAssured.given;
+
+public class UserController {
+
+    private static RequestSpecification givenConfiguredRequest(Cookies cookies) {
+        RequestSpecification specification = given()
+                .baseUri(BASE_URL)
+                .contentType(APPLICATION_JSON)
+                .log().all();
+
+        if (cookies != null) {
+            specification.cookies(cookies);
+        }
+        return specification;
+    }
+
+    public static Response registerUser(UserRegister user) {
+        String bodyUserString = Serializer.convertObjectToJsonString(user);
+        return givenConfiguredRequest(null)
+                .body(bodyUserString)
+                .when()
+                .post(USERS_ENDPOINT)
+                .then().log().body().extract().response();
+    }
+
+    public static Response getUsers(Page page) {
+        String bodyPageString = Serializer.convertObjectToJsonString(page);
+        return givenConfiguredRequest(null)
+                .body(bodyPageString)
+                .when()
+                .post(GET_USERS_BY_NAME_ENDPOINT)
+                .then().log().all().extract().response();
+    }
+
+    public static Response getUserByName(Cookies cookies, SearchUser user) {
+        String bodyGetUserByName = Serializer.convertObjectToJsonString(user);
+        return givenConfiguredRequest(cookies)
+                .body(bodyGetUserByName)
+                .when()
+                .post(GET_USERS_BY_NAME_ENDPOINT)
+                .then().log().all().extract().response();
+    }
+
+    public static Response updatePersonalProfile(Cookies cookies, UserPersonalInfo userPersonal, int currentUserId) {
+        String bodyUpdatedPersonalProfileString = Serializer.convertObjectToJsonString(userPersonal);
+        return givenConfiguredRequest(cookies)
+                .pathParam("currentUserId", currentUserId)
+                .body(bodyUpdatedPersonalProfileString)
+                .when()
+                .post("/api/users/auth/{currentUserId}/personal")
+                .then().log().all().extract().response();
+    }
+}
